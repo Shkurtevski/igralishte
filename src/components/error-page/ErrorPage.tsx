8ee11954c5568269192819c2c1e8c8d55cead2ec
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2023-12-18 13:36:40 +0100

    Added css for Error page

--- src/components/error-page/ErrorPage.tsx
+++ src/components/error-page/ErrorPage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage: React.FC = () => {
   return (
     <React.Fragment>
-      <h1>PAGE NOT FOUND!</h1>;
+      <div className="error-page">
+        <div className="error-page-wrapper">
+          <h2>Страната не е пронајдена!</h2>
+          <Link to="/">
+            <button className="btn btn-black">Кон почетна</button>
+          </Link>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
